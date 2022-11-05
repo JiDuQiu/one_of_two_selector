5249a29ceaea95ccb099737897fb8c3f61a997fb
几度秋 <49580314+JiDuQiu@users.noreply.github.com>
2022-11-05 15:50:33 +0800

    Add files via upload

---
+++ ts_one_of_two_selector.v
@@ -0,0 +1,24 @@
+//2022-7-21,xuan zhe qi
+//选择器；
+
+module fn_sw(a,b,sel,y);
+
+input		a;
+input		b;
+input		sel;
+output		y;
+
+
+//assign y=sel?(a^b):(a&b);
+
+//用always语句块实现组合逻辑；
+reg		y;
+always@(a or b or sel) begin
+	if(sel==1)begin
+		y<=a^b;
+	end
+	else begin
+		y<=a&b;
+	end
+end
+endmodule
